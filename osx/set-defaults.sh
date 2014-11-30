82c4e2b3c702c93282ecd2652b6ebf29e89f19c5
choyer <crhoyer@gmail.com>
2014-11-30 15:20:23 -0500

    add echo line at start. correct comment typo

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -9,10 +9,12 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+echo
+
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+# Keep-alive: update existing `sudo` time stamp until `./set-defaults.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
