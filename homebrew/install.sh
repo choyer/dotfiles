d2a5bf3fea6749377435decf54e25fd4367d03ba
choyer <crhoyer@gmail.com>
2014-11-30 15:23:20 -0500

    fix custom Caskroom folder permission problem

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -91,6 +91,18 @@ echo "Done."
 # sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
 
 echo "Brew cask install ... "
+echo
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Create the Caskroom folder and chown it to avoid permission problems
+mkdir /etc/Caskroom
+sudo chown -R $(whoami) /etc/Caskroom
+
 export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/etc/Caskroom"
 # Put everything into system Applications folder
 brew cask install ${CASKS[@]} && brew cask alfred link
