f4f69dab06fcf9744226283f2d840725da945765
choyer <crhoyer@gmail.com>
2014-11-30 12:51:11 -0500

    do not try to delete sleepimage due to permission issues.

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -488,12 +488,13 @@ defaults write com.apple.appstore ShowDebugMenu -bool true
 # Disable hibernation (speeds up entering sleep mode)
 sudo pmset -a hibernatemode 0
 
+# TODO investigate permission issues when running sleep image commands
 # Remove the sleep image file to save disk space
-sudo rm /Private/var/vm/sleepimage
+#sudo rm /Private/var/vm/sleepimage
 # Create a zero-byte file instead…
-sudo touch /Private/var/vm/sleepimage
+#sudo touch /Private/var/vm/sleepimage
 # …and make sure it can’t be rewritten
-sudo chflags uchg /Private/var/vm/sleepimage
+#sudo chflags uchg /Private/var/vm/sleepimage
 
 # Disable the sudden motion sensor as it’s not useful for SSDs
 sudo pmset -a sms 0
