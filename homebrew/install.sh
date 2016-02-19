5d24fa3cec9d145ceaa04fdac6ce670cf3999e55
choyer <crhoyer@gmail.com>
2016-02-18 23:57:05 -0500

    switch to brew bundle. include linux homebrew install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,109 +9,13 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null 2>&1
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  fi
 fi
-
-# Homebrew taps
-TAPS=(
-homebrew/dupes
-homebrew/versions
-josegonzalez/php
-caskroom/cask
-caskroom/fonts
-caskroom/versions
-)
-
-# Homebrew Formulas
-FORMULAS=(
-coreutils
-zsh
-archey
-wget
-z
-git
-gh
-imagemagick
-todo-txt
-openssl
-caskroom/cask/brew-cask
-)
-
-# Homebrew casks
-CASKS=(
-alfred
-caffeine
-cd-to
-nvalt
-firefox
-opera
-google-chrome
-dash
-imagealpha
-imageoptim
-iterm2
-sequel-pro
-atom
-sublime-text3
-the-unarchiver
-transmit
-vlc
-betterzipql
-qlcolorcode
-qlmarkdown
-qlstephen
-quicklook-csv
-quicklook-json
-webp-quicklook
-transmission
-appcleaner
-istat-menus
-vmware-fusion
-harvest
-trim-enabler
-drobo-dashboard
-)
-
-#================================================================================
-
-echo "Brew update ... "
-brew update
-echo "Done."
-
-echo "Brew tap & install ... "
-brew tap ${TAPS[@]} && brew install ${FORMULAS[@]}
-echo "Done."
-
-
-#echo " Changing shell...."
-# sudo echo "/usr/local/bin/zsh" >> /etc/shells && chsh -s /usr/local/bin/zsh
-
-echo "Brew cask install ... "
-echo
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Create the Caskroom folder and chown it to avoid permission problems
-sudo mkdir /etc/Caskroom
-sudo chown -R $(whoami) /etc/Caskroom
-
-export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/etc/Caskroom"
-# Put everything into system Applications folder
-brew cask install ${CASKS[@]} && brew cask alfred link
-echo "Done."
-
-# 1Password form the Apple Store needs Chrome to be in /Applications
-# Known issues #3
-# https://guides.agilebits.com/1password-mac-kb/5/en/topic/browser-validation-failed
-
-echo "Brew cleanup ... "
-brew cleanup
-echo "Done."
-
-echo "Brew cask cleanup ... "
-brew cask cleanup
-echo "Done."
