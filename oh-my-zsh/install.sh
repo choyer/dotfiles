a1f755906564d6b3e7175269fba95327a77e2cac
Carl Hoyer <crhoyer@gmail.com>
2020-06-19 23:14:16 -0400

    Hello and welcome p10k

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -7,3 +7,6 @@
 
 echo "Downloading & installing oh-my-zsh"
 curl -L http://install.ohmyz.sh | sh
+
+echo "Downloading & installing Powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
