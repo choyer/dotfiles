737281df2f840dbc5f409114a90639fc1858310e
Carl Hoyer <crhoyer@gmail.com>
2020-06-20 21:24:45 -0400

    Add plugin download of zsh-autosuggestions & zsh-syntax-highlighting to oh-my-zsh install

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -10,3 +10,9 @@ curl -L http://install.ohmyz.sh | sh
 
 echo "Downloading & installing Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Downloading & installing zsh-autosuggtions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+echo "Downloading & installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
